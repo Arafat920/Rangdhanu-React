d967b27ff301038dae4d84409a3fb8e15db42196
Md Yeasin Arafat <103861436+Arafat920@users.noreply.github.com>
2026-04-02 17:01:27 +0600

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+function MovieCard({ movie, setSelectedMovie }) {
+  return (
+    <div className="movie-card" onClick={() => setSelectedMovie(movie)}>
+      <img src={IMG_PATH + movie.poster_path} alt={movie.title || movie.name} />
+    </div>
+  );
+}
+
+export default MovieCard;
