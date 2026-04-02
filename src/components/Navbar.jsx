449675c2cebf1da3d609f638c45dd38b3a027e68
Md Yeasin Arafat <103861436+Arafat920@users.noreply.github.com>
2026-04-02 16:59:39 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
 
+function Navbar({ category, setCategory }) {
+  return (
+    <header>
+      <div className="nav-content">
+        <h1 className="logo" onClick={() => setCategory('home')}>RANGDHANU</h1>
+        <nav className="main-nav">
+          <span className={category === 'home' ? 'active' : ''} onClick={() => setCategory('home')}>Home</span>
+          <span className={category === 'tv' ? 'active' : ''} onClick={() => setCategory('tv')}>TV Shows</span>
+          <span className={category === 'movies' ? 'active' : ''} onClick={() => setCategory('movies')}>Movies</span>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
